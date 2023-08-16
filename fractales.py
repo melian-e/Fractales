c5c41f2bd3158ff718f000c005ee3ef3a0a4ccf9
melian-e <iemelian.rambeau@student.junia.com>
2023-08-16 22:15:13 +0200

    final for today

--- fractales.py
+++ fractales.py
@@ -15,6 +15,9 @@ def random_gradiant():
 # fonction qui dessine un triangle
 def triangle(x1, y1, x2, y2, x3, y3, color):
     draw.polygon(fenetre, color, [(x1, y1), (x2, y2), (x3, y3)])
+    # draw.line(fenetre, color, (x1, y1), (x2, y2))
+    # draw.line(fenetre, color, (x2, y2), (x3, y3))
+    # draw.line(fenetre, color, (x3, y3), (x1, y1))
 
 
 # fonction récursive qui dessine des triangles imbriqués
@@ -25,27 +28,68 @@ def triangle_fractale(depth, x1, y1, x2, y2, x3, y3, colors):
         pass
     # sinon, on divise le triangle en 4 triangles plus petits
     else:
-        angle = pi/100
+        angle = pi/200
         rotation_mat = [[cos(angle), -sin(angle)], [sin(angle), cos(angle)]]
         factor = 99/100
 
-        # calcul des coordonnées des points du triangle 2/3 plus petit après une rotaion autour du point (x1, y1)
+        centrex = 500
+        centrey = 500
 
-        x2 = factor*(x2-x1) + x1
-        y2 = factor*(y2-y1) + y1
-        x3 = factor*(x3-x1) + x1
-        y3 = factor*(y3-y1) + y1
-        
-        x2, y2 = x2-x1, y2-y1
-        x3, y3 = x3-x1, y3-y1
-        x2, y2 = rotation_mat[0][0]*x2 + rotation_mat[0][1]*y2, rotation_mat[1][0]*x2 + rotation_mat[1][1]*y2
-        x3, y3 = rotation_mat[0][0]*x3 + rotation_mat[0][1]*y3, rotation_mat[1][0]*x3 + rotation_mat[1][1]*y3
-        x2, y2 = x2+x1, y2+y1
-        x3, y3 = x3+x1, y3+y1
+        centrex2 = (x1+x3)/2
+        centrey2 = (y1+y3)/2
 
-        # appel récursif sur un triangle plus petit avec une rotation de 31degrés (de profondeur n-1)
+        centrex3 = (x2+x3)/2
+        centrey3 = (y2+y3)/2
+
+        x1 = factor*(x1-centrex) + centrex
+        y1 = factor*(y1-centrey) + centrey
+        x2 = factor*(x2-centrex) + centrex
+        y2 = factor*(y2-centrey) + centrey
+        x3 = factor*(x3-centrex) + centrex
+        y3 = factor*(y3-centrey) + centrey
+
+        # calcul des coordonnées des points du triangle plus petit après une rotaion autour du point (x1, y1)
         
-        triangle_fractale(depth-1, x1, y1, x2, y2, x3, y3, colors)
+        
+        x4, y4 = x1-centrex, y1-centrey
+        x5, y5 = x2-centrex, y2-centrey
+        x6, y6 = x3-centrex, y3-centrey
+        x4, y4 = rotation_mat[0][0]*x4 + rotation_mat[0][1]*y4, rotation_mat[1][0]*x4 + rotation_mat[1][1]*y4
+        x5, y5 = rotation_mat[0][0]*x5 + rotation_mat[0][1]*y5, rotation_mat[1][0]*x5 + rotation_mat[1][1]*y5
+        x6, y6 = rotation_mat[0][0]*x6 + rotation_mat[0][1]*y6, rotation_mat[1][0]*x6 + rotation_mat[1][1]*y6
+        x4, y4 = x4+centrex, y4+centrey
+        x5, y5 = x5+centrex, y5+centrey
+        x6, y6 = x6+centrex, y6+centrey
+
+        # calcul des coordonnées des points du triangle plus petit après une rotaion autour du point (x2, y3)
+
+        x7, y7 = x1-centrex2, y1-centrey2
+        x8, y8 = x2-centrex2, y2-centrey2
+        x9, y9 = x3-centrex2, y3-centrey2
+        x7, y7 = rotation_mat[0][0]*x7 + rotation_mat[0][1]*y7, rotation_mat[1][0]*x7 + rotation_mat[1][1]*y7
+        x8, y8 = rotation_mat[0][0]*x8 + rotation_mat[0][1]*y8, rotation_mat[1][0]*x8 + rotation_mat[1][1]*y8
+        x9, y9 = rotation_mat[0][0]*x9 + rotation_mat[0][1]*y9, rotation_mat[1][0]*x9 + rotation_mat[1][1]*y9
+        x7, y7 = x7+centrex2, y7+centrey2
+        x8, y8 = x8+centrex2, y8+centrey2
+        x9, y9 = x9+centrex2, y9+centrey2
+
+        # calcul des coordonnées des points du triangle plus petit après une rotaion autour du point (x3, y3)
+
+        x10, y10 = x1-x3, y1-y3
+        x11, y11 = x2-x3, y2-y3
+        x12, y12 = x3-x3, y3-y3
+        x10, y10 = rotation_mat[0][0]*x10 + rotation_mat[0][1]*y10, rotation_mat[1][0]*x10 + rotation_mat[1][1]*y10
+        x11, y11 = rotation_mat[0][0]*x11 + rotation_mat[0][1]*y11, rotation_mat[1][0]*x11 + rotation_mat[1][1]*y11
+        x12, y12 = rotation_mat[0][0]*x12 + rotation_mat[0][1]*y12, rotation_mat[1][0]*x12 + rotation_mat[1][1]*y12
+        x10, y10 = x10+x3, y10+y3
+        x11, y11 = x11+x3, y11+y3
+        x12, y12 = x12+x3, y12+y3
+
+        # appel récursif
+        triangle_fractale(depth-1, x4, y4, x5, y5, x6, y6, colors)
+        # triangle_fractale(depth-1, x7, y7, x8, y8, x9, y9, colors)
+        # triangle_fractale(depth-1, x10, y10, x11, y11, x12, y12, colors)
+        
 
 # fonction qui dessine une branche
 def branche(x1, y1, x2, y2, color):
@@ -55,7 +99,7 @@ def branche(x1, y1, x2, y2, color):
 fenetre = display.set_mode((1000, 900))
 
 # profondeur de la fractale
-depth = 3000
+depth = 1000
 
 # création des couleurs liées à la profondeur
 colors = []
@@ -78,6 +122,9 @@ else:
 #     colors.append(random_color())
 
 # dessin de la fractale
+triangle_fractale(depth, 500, 450, 0, 1000, 1000, 1000, colors)
+triangle_fractale(depth, 500, 450, 0, 0, 1000, 0, colors)
+triangle_fractale(depth, 500, 450, 0, 0, 0, 1000, colors)
 triangle_fractale(depth, 500, 450, 1000, 0, 1000, 1000, colors)
 
 # mise à jour de l'affichage
